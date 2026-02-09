f9af2a37d91efa2c514684437ada7835fbb6d496
Abdullah Fitri Che Wan <ftk2431@gmail.com>
2026-02-09 17:43:34 +0800

    fix: remove unused useEffect import from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from '@/navigation/AppNavigator';
 import { AuthProvider } from '@/contexts/AuthContext';
